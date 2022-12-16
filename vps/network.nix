791a8af8f9c2b8b5d272ed9f6ceefa12ec2bae54
Dmitry Laptov <dmitry.laptov@serokell.io>
2022-12-16 18:15:54 +0300

    Configure network

--- vps/network.nix
+++ vps/network.nix
@@ -1,27 +1,46 @@
 { config, lib, pkgs, ... }:
 {
   imports = [
+    ./nginx
     ./vpn.nix
-    ./nginx.nix
-    ./containers.nix
   ];
 
   networking = {
     usePredictableInterfaceNames = false;
+    useNetworkd = true;
     hostName = "savely-vps";
     domain = "krendelhoff.space";
-    useDHCP = false;
     nameservers = [
-      "ns1.linode.com"
-      "ns2.linode.com"
-      "ns3.linode.com"
-      "ns4.linode.com"
-      "ns5.linode.com"
+      "139.162.130.5"
+      "139.162.131.5"
+      "139.162.132.5"
+      "139.162.133.5"
+      "139.162.134.5"
+      "139.162.135.5"
+      "139.162.136.5"
+      "139.162.137.5"
+      "139.162.138.5"
+      "139.162.139.5"
+      "2a01:7e01::5"
+      "2a01:7e01::9"
+      "2a01:7e01::7"
+      "2a01:7e01::c"
+      "2a01:7e01::2"
+      "2a01:7e01::4"
+      "2a01:7e01::3"
+      "2a01:7e01::6"
+      "2a01:7e01::b"
+      "2a01:7e01::8"
     ];
-    interfaces.eth0.useDHCP = true;
+    useDHCP = false;
+    interfaces.eth0 = {
+      useDHCP = true;
+      ipv4.addresses = [{ address = "139.162.148.166"; prefixLength = 24; }];
+      ipv6.addresses = [{ address = "2a01:7e01::f03c:93ff:fe38:3ee1"; prefixLength = 64; }];
+    };
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 443 ];
+      allowedTCPPorts = [ 22 80 443 ];
       allowedUDPPorts = [ 7717 ];
       allowedTCPPortRanges = [{
         from = 35000;
